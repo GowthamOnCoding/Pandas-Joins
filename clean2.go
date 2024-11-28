2489502f9adfa77e6941ffe1ae79b17549dab3eb
Gowtham Murugesan <vm.gowtham@hotmail.com>
2024-11-28 19:07:52 +0530

    Create clean2.go

---
+++ clean2.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+)
+
+func main() {
+	inputFileName := "names.csv" // Replace with your input file name
+
+	file, err := os.Open(inputFileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	namesMap := make(map[string]struct{})
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	ch := make(chan string, 100)
+
+	// Start goroutines to process lines
+	numWorkers := 4 // Number of goroutines to use
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for line := range ch {
+				columns := strings.Split(line, ",")
+				if len(columns) >= 5 {
+					givenNameOne := strings.TrimSpace(columns[0])
+					lastName := strings.TrimSpace(columns[4])
+					if givenNameOne != "" && lastName != "" {
+						fullName := strings.ToLower(givenNameOne + " " + lastName)
+						mu.Lock()
+						namesMap[fullName] = struct{}{}
+						mu.Unlock()
+					}
+				}
+			}
+		}()
+	}
+
+	reader := bufio.NewReader(file)
+
+	// Skip the header line
+	_, err = reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			} else if err != nil {
+				fmt.Println("Error reading file:", err)
+				break
+			}
+		}
+		ch <- line
+	}
+	close(ch)
+
+	// Wait for all processing goroutines to finish
+	wg.Wait()
+
+	names := make([]string, 0, len(namesMap))
+	for name := range namesMap {
+		names = append(names, name)
+	}
+
+	sort.Strings(names)
+
+	outputFile, err := os.Create("lookup.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer outputFile.Close()
+
+	writer := bufio.NewWriter(outputFile)
+	for _, name := range names {
+		writer.WriteString(name + "\n")
+	}
+	writer.Flush()
+
+	fmt.Println("Names written to lookup.txt successfully.")
+}
